54ca564e49d4066c91c0de906c3a3f389997a773
NOTyasser <125504642+NOTyasser@users.noreply.github.com>
2024-08-08 01:54:49 +0200

    all

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
 import express from 'express';
-import controllerRouting from './routes/index';
+import router from './routes/index';
 
-const app = express();
+const server = express();
 const port = process.env.PORT || 5000;
 
-app.use(express.json());
-controllerRouting(app);
+server.use(express.json());
+server.use(router);
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
-
-export default app;
